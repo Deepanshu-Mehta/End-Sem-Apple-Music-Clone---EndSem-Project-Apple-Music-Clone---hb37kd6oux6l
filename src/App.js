7caa17dc235e5e4bb9b7a6fa5c9a15ea51dc0459
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-05-20 15:26:27 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import { Navigate, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Nav from "./pages/Nav";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import { useState } from "react";
+import Songs from "./pages/Songs";
 
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const [token, setToken] = useState('');
+  const [buttonText, setButtonText] = useState('');
+  const [buttonPath, setButtonPath] = useState('');
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Str buttonText={buttonText} buttonPath={buttonPath}/>,
+      children: [
+        {
+          path: '/',
+          element: <Home setButtonText={setButtonText} setButtonPath={setButtonPath} />
+        },
+        {
+          path: '/song/:id',
+          element: login ? <Songs token={token}/> : <Navigate replace to="/signin"/>
+        },
+        {
+          path: '/signin',
+          element: <Signin setButtonText={setButtonText} setButtonPath={setButtonPath} setLogin={setLogin} setToken={setToken}/>
+        },
+        {
+          path: '/signup',
+          element: <Signup setButtonText={setButtonText} setButtonPath={setButtonPath} setLogin={setLogin} setToken={setToken}/>
+        }
+      ]
+    }
+  ])
+  function Str({buttonText, buttonPath}) {
+    return (
+      <>
+        <header className="fixed w-full"><Nav buttonText={buttonText} buttonPath={buttonPath}/></header>
+        <main><Outlet/></main>
+      </>
+    )
+  }
+
   return (
     <div className="App">
+      <RouterProvider router={router}/>
     </div>
   );
 }
